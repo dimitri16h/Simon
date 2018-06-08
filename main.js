01e61fcde7320adc92f551a6c479743b42dea390
Dimitri <Dimitri16h@gmail.com>
2018-06-08 11:40:43 -0400

    Logic working

--- main.js
+++ main.js
@@ -1,96 +1,138 @@
 var colorButtons = document.getElementsByClassName("quarter");
+var startButton = document.getElementById("start-button");
 
 var allColors = ["red", "blue", "yellow", "green"];
-var currentGameSequence = []
+var currentGameSequence = [];
+var currentPlayerSequence = [];
 var newColor;
+
+var playerTurn = false;
+var arrowFired = false;
 
 $(window).on('load',function(){
         $('#modal').modal('show');
     });
 
+startButton.addEventListener("click", startButtonLogic);
+
 document.onreadystatechange = function() {
 	if (document.readyState == "interactive") {
 		//Initialize buttons
-		var startButton = document.getElementById("start-button");
+		
 
 		for (var i=0; i < colorButtons.length; i++) {
-			colorButtons[i].addEventListener("click", buttonLogic);
+			colorButtons[i].addEventListener("mousedown", colorButtonLogic);
+			colorButtons[i].addEventListener("mouseup", clearAllLights);
 		}	
-		startButton.addEventListener("click", startGame);
 		window.addEventListener('keydown', arrowKeysLogic);
+		window.addEventListener('keyup', clearAllLights);
 	}
+}
 
-	function buttonLogic(){
-		console.log(this.id);
+function startButtonLogic() {
+	//disable start button once game has started
+	startButton.removeEventListener("click", startButtonLogic);
+
+	checkLevelUp();
+}
+
+function colorButtonLogic(){
+	if (this.id == "redButton") {selectButton("red")}
+	else if (this.id == "yellowButton") {selectButton("yellow")}
+	else if (this.id == "blueButton") {selectButton("blue")}
+	else if (this.id == "greenButton") {selectButton("green")}
+}
+
+function arrowKeysLogic(){
+	if (!arrowFired){
+		if (event.keyCode == 37) {selectButton("yellow")}
+		else if (event.keyCode == 38) {selectButton("red")}
+		else if (event.keyCode == 39) {selectButton("blue")}
+		else if (event.keyCode == 40) {selectButton("green")}
+		arrowFired = true;
 	}
+}
 
-	function arrowKeysLogic(){
-		if (event.keyCode == 37){
-			console.log("yellowButton");
-		}
-        else if (event.keyCode == 38){
-        	console.log("redButton");
-        }
-        else if (event.keyCode == 39) {
-        	console.log("blueButton");
-        }
-        else if (event.keyCode == 40) {
-        	console.log("greenButton");
-        }
+function selectButton(color){
+	if (currentPlayerSequence.length < currentGameSequence.length && playerTurn) {
+		currentPlayerSequence.push(color);
+		lightUp(color);
+		console.log(color, "selected.")
+		console.log(currentPlayerSequence);
 	}
+}
 
-	function selectButton(color){
-		if (color == "red") {
 
-		}
 
-	}
+function checkLevelUp(){
 
-	function startGame() {
+	if (currentPlayerSequence.toString() == currentGameSequence.toString()) {
 		newColor = allColors[Math.floor(Math.random() * 4)];
 		currentGameSequence.push(newColor);
-		showSequence();
 		console.log(currentGameSequence);
-			
-			// flash color
-			// wait for input
-			// end or flash more
+		setTimeout(showSequence, 500, 0);
 	}
 
-	function showSequence() {
-		var i = 0;
-	 	var moves = setInterval(function(){
-	 	clearAllLights();
-	    lightUp(currentGameSequence[i]);
-	    i++;
-	    if (i > currentGameSequence.length) {
-	      clearAllLights();
-	    }
-	  }, 2000)
+	else {
+		console.log("YOU LOSE");
 	}
 
-	function lightUp(color) {
-		if (color=="red"){
-			console.log("Changing reds border");
-			colorButtons[0].style.border="thick solid #FFFFFF";
-		}
-		else if (color=="blue"){
-			console.log("Changing blues border");
-			colorButtons[1].style.border="thick solid #FFFFFF";
-		}
-		else if (color=="yellow"){
-			console.log("Changing yellows border");
-			colorButtons[2].style.border="thick solid #FFFFFF";
-		}
-		else if (color=="green"){
-			console.log("Changing greens border");
-			colorButtons[3].style.border="thick solid #FFFFFF";
-		}
-	}
+}
 
-	function clearAllLights() {
-		for (var i=0; i<4; i++) {
-			colorButtons[i].style.border="0px solid transparent";
-		}
+
+function showSequence(i) {
+	//Passed 0 to start for i, if i < current length, light up 
+	//current color, then set time out to wait 1 second.
+	//after the 1 second, it clears all lights, increments i by 1,
+	//and calls showsequence passing in the new i.
+	if (i<currentGameSequence.length) {
+		lightUp(currentGameSequence[i]);
+		setTimeout(incrementSequence, 1000, i);
+ 	}
+ 	else {
+ 		takePlayerSequence();
+ 	}
+}
+
+function incrementSequence(i) {
+	clearAllLights();
+	i++;
+	setTimeout(showSequence, 200, i);
+}
+
+
+
+function takePlayerSequence() {
+	playerTurn = true;
+	currentPlayerSequence = [];
+}
+
+
+
+
+
+
+function lightUp(color) {
+	if (color=="red"){
+		colorButtons[0].style.border="thick solid #FFFFFF";
+	}
+	else if (color=="blue"){
+		colorButtons[1].style.border="thick solid #FFFFFF";
+	}
+	else if (color=="yellow"){
+		colorButtons[2].style.border="thick solid #FFFFFF";
+	}
+	else if (color=="green"){
+		colorButtons[3].style.border="thick solid #FFFFFF";
+	}
+}
+
+function clearAllLights() {
+	arrowFired = false;
+	for (var i=0; i<4; i++) {
+		colorButtons[i].style.border="0px solid transparent";
+	}
+	if (currentPlayerSequence.length == currentGameSequence.length && currentGameSequence.length != 0) {
+		checkLevelUp();
 	}
 }
